56c632a92a6e5fa5c3aa52ccf5cb50d95c1a17a7
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-09-13 17:44:35 +0200

    Improved defines

--- src/defines.h
+++ src/defines.h
@@ -6,8 +6,10 @@ enum class Os {
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
     #define PLATFORM Os::windows
-#else
+#elif __linux__
     #define PLATFORM Os::linux
+#else
+    error "Unsupported platform"
 #endif
 
 constexpr Os os() noexcept {
