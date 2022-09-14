7163c3f87f038bdd702c99bd56c105f61a0f2fdb
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-09-14 15:35:42 +0200

    Refactored main

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,7 @@ void print(Args...args) {
     std::cout << buffer.str() << std::endl;
 }
 
-int main() {
-
+void demonstrate_multiprocessing() {
     // Showing barrier
     if (Mpi::rank() == Mpi::size()-1) {
         /* We make the last rank print, this way we'll see if the
@@ -22,9 +21,9 @@ int main() {
 
     // Showing async prints
     print("Rank ", Mpi::rank(), " says hello!");
-    Mpi::barrier();
+}
 
-    // Showing broadcast
+void demonstrate_broadcast() {
     {
         int data = Mpi::rank() + 5;
         const auto orig = data;
@@ -32,24 +31,21 @@ int main() {
         print("Rank ", Mpi::rank(), " got ", orig, " -> ", data);
     }
     Mpi::barrier();
-
-    // Showing array broadcast
     {
         std::vector<double> data {Mpi::rank() + 3.141592, Mpi::rank() + 2.71828, Mpi::rank() + 1.61803};
         const auto orig = data;
         Mpi::broadcast(Mpi::size() - 1, data);
         print("Rank ", Mpi::rank(), " got [", orig[0], ", ", orig[1], ", ", orig[2], "] -> [", data[0], ", ", data[1], ", ", data[2], "]");
     }
-    Mpi::barrier();
+}
 
-    // Showing sendrcv
+void demonstrate_sendrecv() {
+    Mpi::id_type sender = 0;
+    Mpi::id_type reciever = Mpi::size() - 1;
+    
+    Mpi::tag_type tag = 500;
+    Mpi::tag_type mock_tag = -1;
     {
-        Mpi::id_type sender = 0;
-        Mpi::id_type reciever = Mpi::size() - 1;
-        
-        Mpi::tag_type tag = 500;
-        Mpi::tag_type mock_tag = -1;
-        
         Mpi::status status;
         status.MPI_TAG = mock_tag;
 
@@ -68,6 +64,17 @@ int main() {
             print("Rank ", Mpi::rank(), " got a message from ", status.MPI_SOURCE, ": ", data);
         }
     }
+}
+
+int main() {
+
+    demonstrate_multiprocessing();
+    Mpi::barrier();
+    
+    demonstrate_broadcast();
+    Mpi::barrier();
+
+    demonstrate_sendrecv();
     Mpi::barrier();
 
 }
