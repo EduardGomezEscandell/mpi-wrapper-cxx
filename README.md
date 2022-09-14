98686880ab0e67d9f3b4f0586b9c9d1de4ac9c91
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-09-14 10:56:54 +0200

    Implemented CI (#1)

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 Simple demo for multi-platform development using a mock MPI implementation on Windows.
 
 # Compile
-MPI is only available on Linux (WSL included!). To compile in windows do
+MPI is only available on Linux (WSL included!).
+
+## Windows
+To compile in windows, install Microsoft Visual Studio and find the path to it. In my case it was `C:\Program Files\Microsoft Visual Studio\2022`. Edit file `configure.bat` and overwrite my path with yours. Then run:
 ```
 .\configure.bat
 ```
--- configure.sh
+++ configure.sh
@@ -6,7 +6,7 @@ cd build
 
 export MPI_ENABLED=${MPI_ENABLED:-"false"}
 
-if [ $MPI_ENABLED==true ]; then
+if [ ${MPI_ENABLED} = "true" ]; then
     export CXX=mpicxx
 else
     export CXX=g++
@@ -15,9 +15,8 @@ fi
 ${CXX} "../src/main.cpp"                                   \
     -Werror -Wall -Wextra -Wpedantic -Wconversion          \
     -O3                                                    \
-    -std=c++20                                             \
+    -std=c++2a                                             \
     -o "../bin/mpidemo"                                    \
     -DMPI_ENABLED=$MPI_ENABLED
 
 echo "Compilation finished with status $?"
-   
