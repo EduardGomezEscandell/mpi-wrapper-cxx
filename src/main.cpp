93a93cf46cb19540ac4b754321ab8afab6ff9267
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-09-14 15:26:04 +0200

    Implemented sendrcv

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,25 @@
 
 #include "mpi_interface.h"
 
+template<typename...Args>
+void print(Args...args) {
+    std::stringstream buffer;
+    ( buffer << ... << args );
+    std::cout << buffer.str() << std::endl;
+}
+
 int main() {
 
     // Showing barrier
     if (Mpi::rank() == Mpi::size()-1) {
         /* We make the last rank print, this way we'll see if the
          barrier fails (as rank 0 tends win data races) */
-        std::cout << "Using MPI processor " << Mpi::processor_name() << std::endl;
+        print("Using MPI processor ", Mpi::processor_name());
     }
     Mpi::barrier();
 
     // Showing async prints
-    {
-        std::string greeting = (std::stringstream{} << "Rank "<< Mpi::rank() << " says hello!\n").str();
-        std::cout << greeting << std::flush;
-    }
+    print("Rank ", Mpi::rank(), " says hello!");
     Mpi::barrier();
 
     // Showing broadcast
@@ -25,8 +29,7 @@ int main() {
         int data = Mpi::rank() + 5;
         const auto orig = data;
         Mpi::broadcast(0, data);
-        std::string msg = (std::stringstream{} << "Rank "<< Mpi::rank() << " got " << orig << " -> " << data << "\n").str();
-        std::cout << msg << std::flush;
+        print("Rank ", Mpi::rank(), " got ", orig, " -> ", data);
     }
     Mpi::barrier();
 
@@ -35,10 +38,35 @@ int main() {
         std::vector<double> data {Mpi::rank() + 3.141592, Mpi::rank() + 2.71828, Mpi::rank() + 1.61803};
         const auto orig = data;
         Mpi::broadcast(Mpi::size() - 1, data);
-        std::string msg = (std::stringstream{} << "Rank "<< Mpi::rank() << " got [" 
-            << orig[0] << ", " << orig[1] << ", " << orig[2] << "] -> ["
-            << data[0] << ", " << data[1] << ", " << data[2] << "]\n").str();
-        std::cout << msg << std::flush;
+        print("Rank ", Mpi::rank(), " got [", orig[0], ", ", orig[1], ", ", orig[2], "] -> [", data[0], ", ", data[1], ", ", data[2], "]");
+    }
+    Mpi::barrier();
+
+    // Showing sendrcv
+    {
+        Mpi::id_type sender = 0;
+        Mpi::id_type reciever = Mpi::size() - 1;
+        
+        Mpi::tag_type tag = 500;
+        Mpi::tag_type mock_tag = -1;
+        
+        Mpi::status status;
+        status.MPI_TAG = mock_tag;
+
+        int data = Mpi::rank();
+        
+        if (Mpi::rank() == sender) {
+            Mpi::send(reciever, tag, data);
+        }
+        if (Mpi::rank() == reciever) {
+            Mpi::recv(sender, tag, data, status);
+        }
+
+        if(status.MPI_TAG == mock_tag) {
+            print("Rank ", Mpi::rank(), " got no message.");
+        } else {
+            print("Rank ", Mpi::rank(), " got a message from ", status.MPI_SOURCE, ": ", data);
+        }
     }
     Mpi::barrier();
 
--- src/mpi_interface.h
+++ src/mpi_interface.h
@@ -2,6 +2,7 @@
 #include <cassert>
 
 #include "defines.h"
+#include <map>
 #include "extra_type_traits.h"
 
 #if MPI_ENABLED==true
@@ -9,29 +10,137 @@
 #endif
 
 template<Os OS, bool MpiEnabled>
-class MpiWrapper {
-};
+class MpiWrapper { };
 
-template<typename T>
-concept ValidMpiType = AnyOf<T,
-    short, unsigned short, int, unsigned int, long, unsigned long, long long, unsigned long long,
-    float, double, long double, char, signed char, unsigned char,   wchar_t, bool>;
 
-template<typename C>
-concept ValidMpiContainer = ContiguousContainer<C> && ValidMpiType<typename container_traits<C>::data>;
+template<Os OS>
+class MpiWrapper<OS, false>;
+
+namespace mpi {
+    using id_type = int;
+    using size_type = int;
+    using tag_type = int;
+
+    template<typename T>
+    concept ValidType = AnyOf<T,
+        short, unsigned short, int, unsigned int, long, unsigned long, long long, unsigned long long,
+        float, double, long double, char, signed char, unsigned char,   wchar_t, bool>;
+
+    template<typename C>
+    concept ValidContainer = ContiguousContainer<C> && ValidType<typename container_traits<C>::data>;
+} // namespace mpi
+
+#if !MPI_ENABLED
+namespace mpi {
+namespace mock {
+
+    struct mpi_status {
+        int count;
+        int cancelled;
+        int MPI_SOURCE;
+        int MPI_TAG;
+        int MPI_ERROR;
+    };
+
+    enum class data_types {
+        short_,
+        unsigned_short_,
+        int_,
+        unsigned_int_,
+        long_,
+        unsigned_long_,
+        long_long_,
+        unsigned_long_long_,
+        float_,
+        double_,
+        long_double_,
+        char_,
+        signed_char_,
+        unsigned_char_,
+        wchar_t_,
+        bool_
+    };
+
+    template<mpi::ValidType T> constexpr data_types mpi_data_type() noexcept;
+    template<> constexpr data_types mpi_data_type<char>()               noexcept { return data_types::char_; }
+    template<> constexpr data_types mpi_data_type<signed char>()        noexcept { return data_types::signed_char_; }
+    template<> constexpr data_types mpi_data_type<unsigned char>()      noexcept { return data_types::unsigned_char_; }
+    template<> constexpr data_types mpi_data_type<wchar_t>()            noexcept { return data_types::wchar_t_; }
+    template<> constexpr data_types mpi_data_type<short>()              noexcept { return data_types::short_; }
+    template<> constexpr data_types mpi_data_type<unsigned short>()     noexcept { return data_types::unsigned_short_; }
+    template<> constexpr data_types mpi_data_type<int>()                noexcept { return data_types::int_; }
+    template<> constexpr data_types mpi_data_type<unsigned int>()       noexcept { return data_types::unsigned_int_; }
+    template<> constexpr data_types mpi_data_type<long>()               noexcept { return data_types::long_; }
+    template<> constexpr data_types mpi_data_type<unsigned long>()      noexcept { return data_types::unsigned_long_; }
+    template<> constexpr data_types mpi_data_type<long long>()          noexcept { return data_types::long_long_; }
+    template<> constexpr data_types mpi_data_type<unsigned long long>() noexcept { return data_types::unsigned_long_long_; }
+    template<> constexpr data_types mpi_data_type<float>()              noexcept { return data_types::float_; }
+    template<> constexpr data_types mpi_data_type<double>()             noexcept { return data_types::double_; }
+    template<> constexpr data_types mpi_data_type<long double>()        noexcept { return data_types::long_double_; }
+    template<> constexpr data_types mpi_data_type<bool>()               noexcept { return data_types::bool_; }
+
+
+    // Type-erased buffer entry
+    class buffer_entry {
+        void * data = nullptr;
+        mpi_status status;
+        mock::data_types type;
+
+    public:
+        template<mpi::ValidType T>
+        buffer_entry(
+            mpi::id_type source,
+            mpi::size_type count,
+            mpi::tag_type tag,
+            T& data) 
+            : data{static_cast<void*>(new T{data})}
+            , type{mpi_data_type<T>()}
+        {
+            status = mpi_status{count, 0, source, tag, 0};
+        }
+
+        buffer_entry(buffer_entry const&) = delete;
+        buffer_entry& operator=(buffer_entry const&) = delete;
+
+        buffer_entry(buffer_entry&& other) { *this = std::forward<buffer_entry>(other); }
+
+        buffer_entry& operator=(buffer_entry&& other) {
+            if(this != &other) {
+                free(this->data);
+                this->data = std::exchange(other.data, nullptr);
+                this->type = other.type;
+                this->status = other.status;
+            }
+            return *this;
+        }
+
+        template<typename T>
+        std::pair<T const&, mpi_status const&> read() const {
+            assert(mpi_data_type<T>() == type);
+            return std::make_pair(*static_cast<T*>(data), status);
+        }
+
+        ~buffer_entry() {
+            free(data);
+        }
+    };
+} // namespace mock
+} // namespace mpi
 
 // Mock implementation for MPI_ENABLED = false
 template<Os OS>
 class MpiWrapper<OS, false> {
   public:
-    using id_type = int;     // It's gross, but it's what MPI uses
-    using size_type = int;
+    using status = mpi::mock::mpi_status;
+    using id_type = mpi::id_type;
+    using size_type = mpi::size_type;
+    using tag_type = mpi::tag_type;
 
-    static constexpr size_type size() noexcept {
+    static constexpr mpi::size_type size() noexcept {
         return 1;
     }
 
-    static constexpr id_type rank() noexcept {
+    static constexpr mpi::size_type rank() noexcept {
         return 0;
     }
     
@@ -41,44 +150,89 @@ class MpiWrapper<OS, false> {
 
     static constexpr void barrier() noexcept { }
 
-    template<ValidMpiType T>
+    template<mpi::ValidType T>
     static constexpr void broadcast(id_type source, T&) {
         assert(source == rank());
     }
 
-    template<ValidMpiContainer T>
+    template<mpi::ValidContainer T>
     static void broadcast(id_type source, T&) {
         assert(source == rank());
     }
 
+    template<mpi::ValidType T>
+    static void send(id_type destination, tag_type tag, T& data) {
+        assert(destination == rank());
+        
+        auto it = sendrcv_buffer.find(tag);
+        auto entry = mpi::mock::buffer_entry{rank(), 1u, tag, data};
+        
+        if(it == sendrcv_buffer.end()) {
+            sendrcv_buffer.emplace(tag, std::move(entry));
+        } else {
+            sendrcv_buffer.at(tag) = std::move(entry);
+        }
+    }
+
+    template<mpi::ValidType T>
+    static void recv(id_type source, tag_type tag, T& data, status& status) {
+        assert(source == rank());
+        auto it = sendrcv_buffer.find(tag);
+        assert(it != sendrcv_buffer.end());
+        auto const& [msg, stat] = it->second.read<T>();
+        data = msg;
+        status = stat;
+        sendrcv_buffer.erase(it);
+    }
+
+    
+
+private:
+    static std::map<tag_type, mpi::mock::buffer_entry> sendrcv_buffer;
 };
 
-#if defined(PLATFORM_IS_LINUX) && MPI_ENABLED
+template<Os OS>
+inline std::map<typename MpiWrapper<OS, false>::tag_type, mpi::mock::buffer_entry> MpiWrapper<OS, false>::sendrcv_buffer{};
+
 
 // Real implementation for MPI_ENABLED = true in Linux
+#elif defined(PLATFORM_IS_LINUX)
+
 template<>
 class MpiWrapper<Os::Linux, true> {
   public:
-    using id_type = int;     // It's gross, but it's what MPI uses
-    using size_type = int;
+    using status = MPI_Status;
+    using id_type = mpi::id_type;
+    using size_type = mpi::size_type;
+    using tag_type = mpi::tag_type;
 
     static std::string processor_name() noexcept;
     static size_type size() noexcept;
     static id_type rank() noexcept;
     static void barrier() noexcept;
 
-    template<ValidMpiType T>
+    template<mpi::ValidType T>
     static void broadcast(id_type source, T& data) {
         MPI_Bcast(&data, 1u, mpi_data_type<T>(), source, MPI_COMM_WORLD);
     }
 
-    template<ValidMpiContainer T>
+    template<mpi::ValidContainer T>
     static void broadcast(id_type source, T& data) {
         MPI_Bcast(&container_traits<T>::front(data),
             static_cast<int>(container_traits<T>::size(data)),
             mpi_data_type<typename container_traits<T>::data>(),
             source,
             MPI_COMM_WORLD);
+    }
+
+    template<mpi::ValidType T>
+    static void send(id_type destination, tag_type tag, T& data) {
+        MPI_Send(&data, 1u, mpi_data_type<T>(), destination, tag, MPI_COMM_WORLD);
+    }
+
+    template<mpi::ValidType T>
+    static void recv(id_type source, tag_type tag, T& data, status& status) {
+        MPI_Recv(&data, 1u, mpi_data_type<T>(), source, tag, MPI_COMM_WORLD, &status);
     }
 
   protected:
@@ -95,7 +249,7 @@ class MpiWrapper<Os::Linux, true> {
 
     static mpi_env env;
 
-    template<ValidMpiType T>
+    template<mpi::ValidType T>
     static constexpr MPI_Datatype mpi_data_type() noexcept;
 };
 
